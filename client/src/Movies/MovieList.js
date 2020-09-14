a245349aca69b83913b8d7292837540614e19fec
JoshuaHolloway <jhollow6@asu.edu>
2020-09-14 15:58:01 -0500

    routing on click of card

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function MovieList(props) {
 
@@ -15,20 +16,25 @@ function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
   return (
-    <div className="movie-card" onClick={ () => {
-          console.log('clicked!');
-          console.log('clicked!');
-          console.log('clicked!');
-      } }>
+
+    <Link to='/movies/1'>
+      <div className="movie-card" onClick={ () => {
+            console.log('clicked!');
+            console.log('clicked!');
+            console.log('clicked!');
+        } }>
+
+        
 
 
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
+    </Link>
   );
 }
